13f85300ada3dd7021e985b17af301639f8b6859
vladu523 <vlad.dumitrescu@tremend.com>
2022-08-15 23:09:36 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {open: 'https://62faa642a1fdf606fb1c193b--eclectic-maamoul-33b51b.netlify.app'},
     plugins: [react()]
 })
