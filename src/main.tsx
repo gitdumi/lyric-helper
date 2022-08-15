52dba0ffe9cdbec9a99783cbbeed5f5d616e20f7
vladu523 <vlad.dumitrescu@tremend.com>
2022-08-15 23:31:43 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
 import "./index.css";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 import {ThemeProvider} from "@emotion/react";
 import {theme} from "./lib/Theme";
 
 ReactDOM.render(
     // <React.StrictMode>
-        <BrowserRouter basename = {process.env.PUBLIC_URL}>
+        <HashRouter basename = {process.env.PUBLIC_URL}>
             <ThemeProvider theme={theme}>
                 <App/>
             </ThemeProvider>
-        </BrowserRouter>,
+        </HashRouter>,
     // </React.StrictMode>,
     document.getElementById('root')
 );
