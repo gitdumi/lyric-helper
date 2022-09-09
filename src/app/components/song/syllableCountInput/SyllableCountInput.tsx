8f28f1d9bbe46d0e9a2b5166c0a5dfc9e64c4022
vladu523 <vlad.dumitrescu@tremend.com>
2022-09-10 00:12:36 +0300

    .

--- src/app/components/song/syllableCountInput/SyllableCountInput.tsx
+++ src/app/components/song/syllableCountInput/SyllableCountInput.tsx
@@ -1,6 +1,6 @@
 import { MAX_SYL_COUNT, MIN_SYL_COUNT, RESPONSIVE_WIDTH } from '../../../../utils/constants';
 import { ClickAwayListener, Input, Tooltip, useMediaQuery } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import React, { MouseEventHandler, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectCurrentSylCount } from '../../../store/dbSlice';
 import useLyricDb from '../../../hooks/dbHook';
@@ -12,6 +12,7 @@ export default function SyllableCountInput(props: { responsive: boolean }) {
   const { setCurrentSylCount } = useLyricDb();
   const currentSylCount = useSelector(selectCurrentSylCount);
   const [value, setValue] = useState<string>(currentSylCount.toString());
+  const [open, setOpen] = useState(false);
   const isResponsive = useMediaQuery(`(max-width: ${RESPONSIVE_WIDTH})`);
 
   useEffect(() => {
@@ -41,17 +42,30 @@ export default function SyllableCountInput(props: { responsive: boolean }) {
 
   // @ts-ignore
   return (
-    <ClickAwayListener onClickAway={() => handleChangeSylCount()}>
+    <ClickAwayListener
+      onClickAway={() => {
+        handleChangeSylCount();
+        setOpen(false);
+      }}
+    >
       <Tooltip
-        placement="bottom"
+        open={open}
+        placement={isResponsive ? 'top' : 'left'}
         title={`pick a value between ${MIN_SYL_COUNT} and ${MAX_SYL_COUNT} to select the number of syllables for newly generated lyrics`}
       >
         <input
           style={{ color: theme.palette.background.paper }}
-          className={responsive ? 'syl-count-input-responsive' : 'syl-count-input'}
+          className={`syl-count-input ${responsive ? '' : 'floating-input'}`}
           type="text"
           maxLength={2}
           value={value}
+          onMouseEnter={() => setOpen(true)}
+          onMouseOver={() => setOpen(true)}
+          onMouseLeave={() => setOpen(false)}
+          onClick={(e: any) => {
+            e.target.value = '';
+            isResponsive ? setOpen(true) : '';
+          }}
           onChange={handleInputChange}
           onFocus={handleFocus}
           onKeyPress={(e) => (e.key === 'Enter' ? handleChangeSylCount() : '')}
